10a51caa1a7db646d3e0a84b50988fecf0502c6a
Divyesh Mali <medivyeshmali@gmail.com>
2024-02-13 19:52:44 +0530

    Built the signin & signup feature for the app using firebase authentication system

---
+++ app/src/main/java/com/divyesh/medanon/UserActivity.java
@@ -0,0 +1,72 @@
+package com.divyesh.medanon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class UserActivity extends AppCompatActivity {
+
+    Button btnLogOut;
+    TextView txtUser;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser user;
+    private FirebaseAuth.AuthStateListener authStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+        firebaseAuth = FirebaseAuth.getInstance();
+        btnLogOut = (Button) findViewById(R.id.btnLogOut);
+        txtUser = (TextView) findViewById(R.id.txtUser);
+        user = firebaseAuth.getCurrentUser();
+
+        if (user == null ) {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            txtUser.setText(user.getEmail());
+        }
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                FirebaseAuth.getInstance().signOut();
+                //Intent I = new Intent(UserActivity.this, LoginActivity.class);
+                //startActivity(I);
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+    }
+}
+
+
+
+//package com.divyesh.medanon;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.os.Bundle;
+//
+//public class UserActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_user);
+//    }
+//}
